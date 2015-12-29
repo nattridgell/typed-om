dd9766b55a12b5788e390bca4ebc0239af8e299f
Natalie Tridgell <tridgell@google.com>
2015-12-29 16:39:54 +1100

    Add some tests for transform value

--- test/js/transform-value.js
+++ test/js/transform-value.js
@@ -77,6 +77,24 @@ suite('TransformValue', function() {
     assert.deepEqual(transformMatrix, expectedMatrix);
   });
 
+  function assertTransformComponents(actualTransform, expectedComponents, errorMsg) {
+    assert.isNotNull(actualTransform, errorMsg);
+    assert.instanceOf(actualTransform, TransformValue, errorMsg);
+    var actualComponents = actualTransform.transformComponents;
+    assert.strictEqual(actualComponents.length, expectedComponents.length,
+        errorMsg + ' Different number of components.');
+    for (var i = 0; i < expectedComponents.length; i++) {
+      var actual = actualComponents[i];
+      var expected = expectedComponents[i];
+      assert.instanceOf(actual, TransformComponent, errorMsg);
+      assert.strictEqual(actual.cssString, expected.cssString, errorMsg);
+      assert.strictEqual(actual.is2DComponent(), expected.is2DComponent(),
+          errorMsg);
+      assert.deepEqual(actual.asMatrix(), expected.asMatrix(), errorMsg);
+      assert.deepEqual(actual, expected, errorMsg);
+    }
+  }
+
   test('TransformValue constructor works with multiple 2D and 3D components',
         function() {
     var transform;
@@ -99,5 +117,124 @@ suite('TransformValue', function() {
     var transformMatrix = transform.asMatrix();
     assert.strictEqual(transformMatrix.cssString, expectedMatrix.cssString);
     assert.deepEqual(transformMatrix, expectedMatrix);
+  });
+
+  test('TransformValue.parse returns expected transformComponents for single ' +
+      'component strings', function() {
+    var simpleLength = new SimpleLength(1, 'px');
+    var values = [
+      // Simple components
+      {str: 'matrix3d(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)',
+          out: new Matrix(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)},
+      {str: 'matrix(0, 1, 2, 3, 4, 5)', out: new Matrix(0, 1, 2, 3, 4, 5)},
+      {str: 'perspective(1px)', out: new Perspective(simpleLength)},
+      {str: 'rotate3d(1, 2, 3, 0deg)', out: new Rotation(0, 1, 2, 3)},
+      {str: 'rotate(0deg)', out: new Rotation(0)},
+      {str: 'scale3d(0, 1, 2)', out: new Scale(0, 1, 2)},
+      {str: 'scale(0, 1)', out: new Scale(0, 1)},
+      {str: 'skew(0, 0)', out: new Skew(0, 0)},
+      {str: 'translate3d(1px, 1px, 1px)',
+          out: new Translation(simpleLength, simpleLength, simpleLength)},
+      {str: 'translate(1px, 1px)',
+          out: new Translation(simpleLength, simpleLength)},
+
+      // No spacing
+      {str: 'matrix3d(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)',
+          out: new Matrix(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)},
+      {str: 'translate3d(1px,1px,1px)',
+          out: new Translation(simpleLength, simpleLength, simpleLength)},
+
+      // Extra spacing
+      {str: ' matrix(0, 1, 2, 3, 4, 5) \n ', out: new Matrix(0, 1, 2, 3, 4, 5)},
+      {str: 'translate( \t 1px, \n 1px \n)',
+          out: new Translation(simpleLength, simpleLength)},
+    ];
+
+    for (var i = 0; i < values.length; i++) {
+      var result = TransformValue.parse(values[i].str);
+      var errorMsg = 'Parsing ' + values[i].str + ' did not produce the ' +
+          'expected TransformComponent.';
+      assertTransformComponents(result, [values[i].out], errorMsg);
+    }
+  });
+
+  test('TransformValue.parse returns expected transformComponents for ' +
+      'compound component strings', function() {
+    var simpleLength = new SimpleLength(1, 'px');
+    var components = {
+      matrix3d: new Matrix(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+      matrix0: new Matrix(0, 1, 2, 3, 4, 5),
+      matrix1: new Matrix(1, 1, 1, 1, 1, 1),
+      perspective: new Perspective(simpleLength),
+      rotate3d: new Rotation(0, 1, 2, 3),
+      rotate: new Rotation(0),
+      scale0: new Scale(0, 1),
+      scale1: new Scale(10, -2),
+      skew: new Skew(10, 15),
+      translate3d: new Translation(simpleLength, simpleLength, simpleLength),
+      translate: new Translation(simpleLength, simpleLength),
+    };
+
+    var values = [
+      // Repetition of the same type
+      {str: 'matrix(0, 1, 2, 3, 4, 5) matrix(1, 1, 1, 1, 1, 1)',
+          out: [components.matrix0, components.matrix1]},
+      {str: 'scale(0, 1) scale(10, -2)',
+          out: [components.scale0, components.scale1]},
+      {str: 'scale(0, 1) scale(10, -2) scale(0, 1)',
+          out: [components.scale0, components.scale1, components.scale0]},
+
+      // Mixture of 3d and 2d of same type.
+      // Must test both 3d before 2d and vice versa.
+      {str: 'matrix3d(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15) ' +
+          'matrix(0, 1, 2, 3, 4, 5)',
+          out: [components.matrix3d, components.matrix0]},
+      {str: 'matrix(0, 1, 2, 3, 4, 5) ' +
+          'matrix3d(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)',
+          out: [components.matrix0, components.matrix3d]},
+      {str: 'rotate3d(1, 2, 3, 0deg) rotate(0deg)',
+          out: [components.rotate3d, components.rotate]},
+      {str: 'rotate(0deg) rotate3d(1, 2, 3, 0deg)',
+          out: [components.rotate, components.rotate3d]},
+      {str: 'translate3d(1px, 1px, 1px) translate(1px, 1px)',
+          out: [components.translate3d, components.translate]},
+      {str: 'translate(1px, 1px) translate3d(1px, 1px, 1px)',
+          out: [components.translate, components.translate3d]},
+
+      // Complex mixture of types
+
+      // No spacing
+      {str: }
+
+    ];
+
+    for (var i = 0; i < values.length; i++) {
+      var result = TransformValue.parse(values[i].str);
+      var errorMsg = 'Parsing ' + values[i].str + ' did not produce the ' +
+          'expected TransformComponent.';
+      assertTransformComponents(result, values[i].out, errorMsg);
+    }
+  });
+
+  test('TransformValue.parse throws exceptions for invalid input.', function() {
+    var values = [
+      // Invalid types.
+      null, 5, {},
+      // Completely invalid strings.
+      '', '5px',
+      // Invalid number of arguments.
+      // Swapping number of arguments between 2d and 3d versions.
+      'matrix3d(0, 1, 2, 3, 4, 5)', 'rotate(1, 2, 3, 0deg)'
+      // Invalid numbers
+      '-3.4e-2.6px',
+      // Invalid calc statements.
+      'calc()', 'calc(5)', 'calc(50 + 5px)', 'calc(pickles)',
+      'calc(5px + 5invalid)', 'calc(5px * 5px)',
+      // Invalid or missing units.
+      '100', '50somethings'
+    ];
+    for (var i = 0; i < values.length; i++) {
+      assert.throws(function() { LengthValue.parse(values[i]); }, TypeError);
+    }
   });
 });
